07adb6d13ad946c8cf6093a3a89f9cf7e8d9ab15
Cefalu <mcefalu@rand.org>
2019-05-05 21:42:58 -0600

    updated iptw to work with new ps function. this closes #10.

--- R/iptw.R
+++ R/iptw.R
@@ -1,110 +1,103 @@
 
-iptw <- function(formula, data, timeInvariant = NULL, n.trees = 10000, stop.method = "es.max", cumulative = TRUE, timeIndicators = NULL, ID = NULL, priorTreatment = TRUE,  ...){
-	if(!is.list(formula) & is.null(timeIndicators)) stop("\"formula\" must be a list with length equal to the number of time points (wide data format), or timeIndicators must be specified (long data format).")
-	
-	if(length(stop.method) > 1) stop("Only one stop.method is allowed at one time.") 
-	
-	isLong <- !is.list(formula)      
-	estimand <- NULL
-	
-	if(isLong){
-		if(length(grep(".time.", attr(terms(formula), "term.labels"))) > 0){
-			stop("For long-format data specifications, \".time.\" is not allowed in variable names in the \n
-		right-hand side of the formula. Please rename: \n", attr(terms(formula), "term.labels")[grep(".time.", attr(terms(formula), "term.labels"))])
-		}
-	}
-	else{
-		wideDat <- NULL
-	}
-	
-	if(isLong){
-		if(is.null(ID)) warning("Using long data format specification without IDs. The function assumes \n that the ordering of subjects in 'data' is the same at all time points.")
-		tvCov <- attr(terms(formula), "term.labels")
-		if(!is.null(timeInvariant)) tiCov <- attr(terms(timeInvariant), "term.labels")
-		tb <- table(timeIndicators)
-		tb <- tb[tb > 0]
-		if(!(max(tb) == min(tb))) stop("Outcomes must be observed at all times for all subjects.")
-		unqTimes <- sort(unique(timeIndicators))
-		formList <- vector(mode = "list", length = length(unqTimes))
-		hldDat <- subset(data, timeIndicators == unqTimes[1])
-		trtVar <- all.vars(formula)[1]
-		hldDat <- hldDat[,names(hldDat) %in% c(tvCov, trtVar)]
-		hdNm <- names(hldDat)
-		names(hldDat) <- paste(hdNm, ".time.", unqTimes[1], sep = "")
-		trtVarLong <- paste(trtVar, ".time.", unqTimes[1], sep = "")
-		tvCovLong <- paste(tvCov, ".time.", unqTimes[1], sep = "")
-		formList[[1]] <- as.formula(paste(trtVarLong, paste(tvCovLong, collapse = " + "), sep= "~"))
-		wideDat <- hldDat
-		for(i in 2:length(unqTimes)){
-			hldDat <- subset(data, timeIndicators == unqTimes[i])
-			hldDat <- hldDat[,names(hldDat) %in% c(tvCov, trtVar)]
-			names(hldDat) <- paste(hdNm, ".time.", unqTimes[i], sep = "")
-			wideDat <- cbind(wideDat, hldDat)
-			trtVarLong <- paste(trtVar, ".time.", unqTimes[i], sep = "")
-			tvCovLong <- paste(tvCov, ".time.", unqTimes[i], sep = "")
-			formList[[i]] <- as.formula(paste(trtVarLong, paste(tvCovLong, collapse = " + "), sep= "~"))
-		}
-		dt2 <- wideDat
-		
-	}
-	else{
-		unqTimes <- 1:length(formula)
-		formList <- formula
-		dt2 <- data
-	}
-	
-	if(! is.null(timeInvariant)){
-		invTerms <- attr(terms(timeInvariant), "term.labels")
-		for(i in 1:length(formula)){
-			currTerms <- union(attr(terms(formList[[i]]), "term.labels"), invTerms)
-			#formList[[i]] <- as.formula(paste(all.vars(formList[[i]])[1], paste(invTerms, collapse = " + "), sep = "~"))
-			formList[[i]] <- as.formula(paste(all.vars(formList[[i]])[1], paste(currTerms, collapse = " + "), sep = "~"))
-
-		}
-	}
-
-
-	nFits <- length(formList)
-	
-	if(priorTreatment) {
-		for(i in 2:nFits){
-			oldTerms <- attr(terms(formList[[i]]), "term.labels")
-			allTerms <- c(oldTerms, attr(terms(formList[[i-1]]), "variables")[[2]])
-			allTerms <- allTerms[!duplicated(allTerms)]
-			formList[[i]] <- as.formula(paste(all.vars(formList[[i]])[1], paste(allTerms, collapse = " + "), sep = "~"))
-		}
-	}		
-	
-	if(cumulative){
-		for(i in 2:nFits){
-			oldTerms <- attr(terms(formList[[i-1]]), "term.labels")
-			allTerms <- union(oldTerms, attr(terms(formList[[i]]), "term.labels"))
-			formList[[i]] <- as.formula(paste(all.vars(formList[[i]])[1], paste(allTerms, collapse = " + "), sep = "~"))
-		}
-	}
-	
-
-	
-	formula <- formList	
-	
-	y1 <- model.extract(model.frame(formula[[1]], data = dt2) , component = "response")
-	if(all(y1 %in% c(0,1))) fitType <- 1
-	else if(is.factor(y1)) fitType <- 2
-	else stop("Treatment variable must either be 0/1 or a factor.")
-	
-	psList <- vector(mode = "list", length = length(formula))
-	
-		
-	for(i in 1:nFits){
-		if(fitType == 1) psList[[i]] <- ps(formula[[i]], data = dt2, n.trees = n.trees, stop.method = stop.method, estimand = "ATE", ...)	
-		if(fitType == 2) psList[[i]] <- mnps(formula[[i]], data = dt2, n.trees = n.trees, stop.method = stop.method, estimand = "ATE", ...)	
-	}
-	
-	outObj <- list(psList = psList, stopMethods = stop.method, nFits = nFits, 
-	uniqueTimes = unqTimes)
-	
-	if(fitType ==1) class(outObj) <- "iptw"
-	if(fitType == 2) class(outObj) <- "mniptw"
-	return(outObj)
+iptw <- function(formula, 
+                 data,  
+                 # iptw options
+                 timeInvariant = NULL,
+                 cumulative = TRUE, 
+                 timeIndicators = NULL, 
+                 ID = NULL, 
+                 priorTreatment = TRUE,
+                 # boosting options -- gbm version first, then xgboost name
+                 n.trees=10000, nrounds=n.trees,
+                 interaction.depth=3, max_depth=interaction.depth,
+                 shrinkage=0.01, eta=shrinkage , 
+                 bag.fraction = 1.0, subsample=bag.fraction,
+                 params=NULL,
+                 perm.test.iters=0,
+                 print.level=2,       
+                 verbose=TRUE,
+                 stop.method = c("es.max"), 
+                 sampw = NULL, 
+                 version="fast",
+                 ks.exact=NULL,
+                 booster="gbm",
+                 tree_method="hist",
+                 save.propensities=FALSE,
+                 file=NULL,
+                 n.keep = 1,
+                 n.grid = 25,
+                 ...){
+   
+   
+   ## throw some errors if the user specifies two versions of the same option
+   if (!missing(n.trees) & !missing(nrounds)) stop("Only one of n.trees and nrounds can be specified.")
+   if (!missing(interaction.depth) & !missing(max_depth)) stop("Only one of interaction.depth and max_depth can be specified.")
+   if (!missing(shrinkage) & !missing(eta)) stop("Only one of shrinkage and eta can be specified.")
+   if (!missing(bag.fraction) & !missing(subsample)) stop("Only one of shrinkage and eta can be specified.")
+   
+   # throw error if user specifies params with other options
+   if (!missing(interaction.depth) | !missing(max_depth) | !missing(shrinkage) | !missing(eta) | !missing(bag.fraction) | !missing(subsample) ){
+      if (!is.null(params)) stop("params cannot be specified with any of interaction.depth, max_depth, shrinkage, eta, bag.fraction, or subsample.")
+   }
+   
+   if (version=="legacy"){
+      ## throw some errors if the user specifies an option not allowed in legacy version of ps
+      if (!is.null(ks.exact))     stop("Option ks.exact is not allowed with version='legacy'")
+      if (!is.null(params))     stop("Option params is not allowed with version='legacy'")
+      if (!missing(booster))      stop("Option booster is not allowed with version='legacy'")
+      if (!missing(tree_method))  stop("Option tree_method is not allowed with version='legacy'")
+      if (!missing(n.keep))       stop("Option n.keep is not allowed with version='legacy'")
+      if (!missing(n.grid))       stop("Option n.grid is not allowed with version='legacy'")
+      
+      return(iptw.old(formula = formula,
+                      data=data,                         # data
+                      # iptw options
+                      timeInvariant = timeInvariant,
+                      cumulative = cumulative, 
+                      timeIndicators = timeIndicators, 
+                      ID = ID, 
+                      priorTreatment = priorTreatment,
+                      # gbm options
+                      n.trees=nrounds,                 
+                      interaction.depth=max_depth,
+                      shrinkage=eta,
+                      bag.fraction = subsample,
+                      perm.test.iters=perm.test.iters,
+                      print.level=print.level,       
+                      verbose=verbose,
+                      stop.method = stop.method, 
+                      sampw = sampw, 
+                      ...))
+   }else{
+      # throw error if user specifies params with booster=="gbm"
+      if ( booster=="gbm" & !is.null(params) ) stop("params cannot be specified when booster='gbm'.")
+      return(iptw.fast(formula = formula,
+                       data=data,                         # data
+                       # iptw options
+                       timeInvariant = timeInvariant,
+                       cumulative = cumulative, 
+                       timeIndicators = timeIndicators, 
+                       ID = ID, 
+                       priorTreatment = priorTreatment,
+                       # boosting options
+                       n.trees=nrounds,                 
+                       interaction.depth=max_depth,
+                       shrinkage=eta,
+                       bag.fraction = subsample,
+                       params=params,
+                       perm.test.iters=perm.test.iters,
+                       print.level=print.level,       
+                       verbose=verbose,
+                       stop.method = stop.method, 
+                       sampw = sampw, 
+                       ks.exact=ks.exact,
+                       booster=booster,
+                       tree_method=tree_method,
+                       save.propensities=save.propensities,
+                       file=file,
+                       n.keep = n.keep,
+                       n.grid = n.grid,
+                       ...))
+   }
 	
 }
