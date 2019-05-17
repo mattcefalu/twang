04d0d67a0a284cbc864c63a027d7888c0b0cb71a
jbiggsets <jbiggs@ets.org>
2019-05-17 09:37:53 -0400

    Various updates, including: - Updates to DESCRIPTION to allow the use of markdown syntax. - Updates to `bal.table`, `dx.wts`, `get.weights`, `iptw`, and `ps` documentation - Generation of new Rd files for the above.

--- R/iptw.R
+++ R/iptw.R
@@ -1,4 +1,80 @@
-
+#' Inverse probability of treatment weighting for marginal structural models.
+#'
+#' `iptw` uses [gbm] to estimate propensity scores for sequential treatments.
+#'
+#' This function uses generalized boosted models to estimate inverse probability of
+#' treatment weights for sequential treatments.
+#'
+#' @param formula Either a single formula (long format) or a list with formulas.
+#' @param data The dataset, includes treatment assignment as well as covariates.
+#' @param timeInvariant An optional formula (with no left-hand variable) specifying time-invariant
+#'   chararacteristics.
+#' @param cumulative If `TRUE`, the time t model includes time-varying characteristics from times 1
+#'   through t-1. Default: `TRUE`.
+#' @param timeIndicators For long format fits, a vector of times for each observation.
+#' @param ID For long format fits, a vector of numeric identifiers for unique analytic units.
+#' @param priorTreatment For long format fits, includes treatment levels from previous times if `TRUE`. This
+#'   argument is ignored for wide format fits. Default: `TRUE`.
+#' @param n.trees Number of gbm iterations passed on to [gbm].
+#' @param interaction.depth A positive integer denoting the tree depth used in
+#'   gradient boosting. Only one of `interaction.depth` and
+#'   `max_depth` can be specified. Default: 3.
+#' @param shrinkage A numeric value between 0 and 1 denoting the learning rate.
+#'   See [gbm] for more details. Only one of `shrinkage`
+#'   and `eta` can be specified. Default: 0.01.
+#' @param bag.fraction A numeric value between 0 and 1 denoting the fraction of
+#'   the observations randomly selected in each iteration of the gradient
+#'   boosting algorithm to propose the next tree. See [gbm] for
+#'   more details. Only one of `bag.fraction` and `subsample` can be
+#'   specified. Default: 1.0.
+#' @param params ...
+#' @param perm.test.iters A non-negative integer giving the number of iterations
+#'   of the permutation test for the KS statistic. If `perm.test.iters=0`
+#'   then the function returns an analytic approximation to the p-value. Setting
+#'   `perm.test.iters=200` will yield precision to within 3\% if the true
+#'   p-value is 0.05. Use `perm.test.iters=500` to be within 2\%. Default: 0.
+#' @param print.level The amount of detail to print to the screen. Default: 2.
+#' @param verbose If `TRUE`, lots of information will be printed to monitor the
+#'   the progress of the fitting. Default: `TRUE`.
+#' @param stop.method A method or methods of measuring and summarizing balance across pretreatment
+#'   variables. Current options are `ks.mean`, `ks.max`, `es.mean`, and `es.max`. `ks` refers to the
+#'   Kolmogorov-Smirnov statistic and es refers to standardized effect size. These are summarized
+#'   across the pretreatment variables by either the maximum (`.max`) or the mean (`.mean`). 
+#'   Default: `c("es.max")`.
+#' @param sampw Optional sampling weights.
+#' @param version Legacy?
+#' @param ks.exact `NULL` or a logical indicating whether the
+#'   Kolmogorov-Smirnov p-value should be based on an approximation of exact
+#'   distribution from an unweighted two-sample Kolmogorov-Smirnov test. If
+#'   `NULL`, the approximation based on the exact distribution is computed
+#'   if the product of the effective sample sizes is less than 10,000.
+#'   Otherwise, an approximation based on the asymptotic distribution is used.
+#'   **Warning:** setting `ks.exact = TRUE` will add substantial
+#'   computation time for larger sample sizes. Default: `NULL`.
+#' @param booster `gbm` or `xgboost`. Default: `gbm`.
+#' @param tree_method `xgboost` param. Default: "hist".
+#' @param save.propensities Whether to save the propensity scores. Default: `FALSE`.
+#' @param file Optional RDS file path where the `ps` object will be saved.
+#' @param n.keep A numeric variable indicating the algorithm should only
+#'   consider every `n.keep`-th iteration of the propensity score model and
+#'   optimize balance over this set instead of all iterations. Default: 1.
+#' @param n.grid A numeric variable that sets the grid size for an initial
+#'   search of the region most likely to minimize the `stop.method`. A
+#'   value of `n.grid=50` uses a 50 point grid from `1:n.trees`. It
+#'   finds the minimum, say at grid point 35. It then looks for the actual
+#'   minimum between grid points 34 and 36. Default: 25.
+#' @param ... Additional arguments that are passed to ps function.
+#'
+#' @return Returns an object of class `iptw`, a list containing
+#'   * `psList` A list of `ps` objects with length equal to the number of time periods.
+#'   * `estimand` The specified estimand.
+#'   * `stop.methods` The stopping rules used to optimize `iptw` balance.
+#'   * `nFits` The number of `ps` objects (i.e., the number of distinct time points).
+#'   * `uniqueTimes` The unique times in the specified model.
+#'
+#' @seealso [ps]
+#'
+#' @export
 iptw <- function(formula, 
                  data,  
                  # iptw options
@@ -8,22 +84,22 @@ iptw <- function(formula,
                  ID = NULL, 
                  priorTreatment = TRUE,
                  # boosting options -- gbm version first, then xgboost name
-                 n.trees=10000, nrounds=n.trees,
-                 interaction.depth=3, max_depth=interaction.depth,
-                 shrinkage=0.01, eta=shrinkage , 
-                 bag.fraction = 1.0, subsample=bag.fraction,
-                 params=NULL,
-                 perm.test.iters=0,
-                 print.level=2,       
-                 verbose=TRUE,
+                 n.trees = 10000, nrounds = n.trees,
+                 interaction.depth = 3, max_depth = interaction.depth,
+                 shrinkage = 0.01, eta = shrinkage , 
+                 bag.fraction = 1.0, subsample = bag.fraction,
+                 params = NULL,
+                 perm.test.iters = 0,
+                 print.level = 2,       
+                 verbose = TRUE,
                  stop.method = c("es.max"), 
                  sampw = NULL, 
-                 version="fast",
-                 ks.exact=NULL,
-                 booster="gbm",
-                 tree_method="hist",
-                 save.propensities=FALSE,
-                 file=NULL,
+                 version = "fast",
+                 ks.exact = NULL,
+                 booster = "gbm",
+                 tree_method = "hist",
+                 save.propensities = FALSE,
+                 file = NULL,
                  n.keep = 1,
                  n.grid = 25,
                  ...){
