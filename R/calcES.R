6e4b357fac30930033894f7c050db398b1ed9a59
ngreifer <noah.greifer@gmail.com>
2019-06-17 15:28:27 -0700

    Added moderator argument, which causes effects to be computed overall and within each subgroup of the moderator (if any)

--- R/calcES.R
+++ R/calcES.R
@@ -1,62 +1,111 @@
-calcES <- function(data,treat,w,numeric.vars,estimand,multinom=FALSE,sw=NULL,get.means=FALSE){
+calcES <- function(data,treat,w,numeric.vars,estimand,multinom=FALSE,sw=NULL,get.means=FALSE, moderator.var = NULL){
 
   # with get.means, only one weight can be specified 
   if (get.means){
     if (  ifelse( is.null(ncol(w)) , FALSE , ncol(w)>1) ){
-      stop("Only a single weight can be specified wtih gen.means=TRUE")
+      stop("Only a single weight can be specified wtih get.means=TRUE")
     }
   }
   
-  N = nrow(data)
-  N1 = sum(treat)
-  N0 = N-N1
+  if (!is.null(moderator.var)) {
+    moderator = data[,moderator.var]
+    groups <- c("all", levels(moderator))
+    data <- data[,!colnames(data) %in% moderator.var, drop = FALSE]
+  }
+  else {
+    groups <- ""
+  }
   
-  # separate out treatment group for easy matrix operations
-  w1 = w[treat==1,]                  # subset to treated weight matrix
-  data1 = as.matrix(data[treat==1,]) # subset to treated data
-  index.1 = !is.na(data1)            # only numeric variable will have missing data, which are dropped from ES calculation.
-  den1 = t(w1)%*%index.1             # sum of weights changes depending on missing data -- only numeric variables should have missing values in data
-  data1[!index.1] = 0                # change missing values to 0 so they dont contribute to the sum and dont mess up matrix operations
-  m1 = t(w1)%*%data1 / den1          # calculate mean among treated
-  
-
-  
-  # m0 and the SD depends on estimand
-  if (estimand=="ATE"){
-    # need variance based only on sw
-    data = as.matrix(data)
-    index = !is.na(data)
-    data[!index] = 0         # change missing values to 0 so they dont contribute to the sum and dont mess up matrix operations
-    den = sw%*%index
-    m.sw = sw%*%data / den
-    
-    # pooled variance estimate for ATE
-    var.p = sw%*%data^2/den - (m.sw)^2
-    var.p = ifelse(var.p==0 , NA , var.p)
-    var.p[,numeric.vars] =  var.p[,numeric.vars] * N / (N-1)
-    
-    if (get.means){
-      # calculate E[X^2] among treated
-      m1.2 =  t(w1)%*%(data1^2) / den1 
-      var1=(m1.2-m1^2) 
-      var1 = ifelse(var1==0 , NA , var1)
-      var1[,numeric.vars] =  var1[,numeric.vars] * N1 / (N1-1)
+  #Get list of std.effects for each moderator group
+  std.effect.list <- lapply(groups, function(g) {
+    if (g %in% c("", "all")) { #overall sample
+      in. <- rep(TRUE, nrow(data))
+      data_ <- data
+      if (g == "") g_name <- ""
+      else g_name <- paste0(g, "_")
+    }
+    else { #subgroup-specific sample
+      in. <- moderator == g
+      #Drop moderator variable from group-specific balance
+      data_ <- data[,!colnames(data) %in% paste(moderator.var, levels(moderator), sep = ":"), drop = FALSE]
+      g_name <- paste0(g, "_")
     }
     
-    if (multinom){
-      # multinom ATE: m0 is full population mean
-      m0 = m.sw
+    #in. means "in the group under analysis". With no moderator and for the overall sample, this is TRUE for everyone.
+    
+    N = sum(in.)
+    N1 = sum(treat[in.])
+    N0 = N-N1
+    
+    # separate out treatment group for easy matrix operations
+    w1 = w[in. & treat==1,,drop = FALSE]                  # subset to treated weight matrix
+    data1 = as.matrix(data_[in. & treat==1,,drop = FALSE]) # subset to treated data
+    index.1 = !is.na(data1)            # only numeric variable will have missing data, which are dropped from ES calculation.
+    den1 = t(w1) %*% index.1             # sum of weights changes depending on missing data -- only numeric variables should have missing values in data
+    data1[!index.1] = 0                # change missing values to 0 so they dont contribute to the sum and dont mess up matrix operations
+    m1 = t(w1) %*% data1 / den1          # calculate mean among treated
+    
+    
+    
+    # m0 and the SD depends on estimand
+    if (estimand=="ATE"){
+      # need variance based only on sw
+      data_g = as.matrix(data_[in.,,drop = FALSE])
+      sw_g <- sw[in.]
+      index = !is.na(data_g)
+      data_g[!index] = 0         # change missing values to 0 so they dont contribute to the sum and dont mess up matrix operations
+      den = sw_g %*% index
+      m.sw = sw_g %*% data_g / den
+      
+      # pooled variance estimate for ATE
+      var.p = sw_g %*% data_g ^ 2 / den - (m.sw)^2
+      var.p = ifelse(var.p==0 , NA_real_ , var.p)
+      var.p[,numeric.vars] =  var.p[,numeric.vars,drop = FALSE] * N / (N-1)
       
       if (get.means){
-        var0=var.p
+        # calculate E[X^2] among treated
+        m1.2 =  t(w1)%*%(data1^2) / den1 
+        var1=(m1.2-m1^2) 
+        var1 = ifelse(var1==0 , NA_real_ , var1)
+        var1[,numeric.vars] =  var1[,numeric.vars,drop = FALSE] * N1 / (N1-1)
       }
       
-      # this just handles that m0 and var.p are vectors when using multinom option
-      std.effect = sweep(sweep( m1 , 2 , sqrt(var.p) , "/") , 2 , m0/sqrt(var.p) , "-")
-    }else{
+      if (multinom){
+        # multinom ATE: m0 is full population mean
+        m0 = m.sw
+        
+        if (get.means){
+          var0=var.p
+        }
+        
+        # this just handles that m0 and var.p are vectors when using multinom option
+        std.effect = sweep(sweep( m1 , 2 , sqrt(var.p) , "/") , 2 , m0/sqrt(var.p) , "-")
+      }else{
+        # same steps as with treated, but now for controls
+        w0 = w[in. & treat==0,,drop = FALSE]
+        data0 = as.matrix(data_[in. & treat==0,,drop = FALSE])
+        index.0 = !is.na(data0)
+        den0 = t(w0)%*%index.0
+        data0[!index.0] = 0
+        
+        # calculate mean among control
+        m0 = t(w0)%*%data0 / den0
+        
+        if (get.means){
+          # calculate E[X^2] among control
+          m0.2 =  t(w0)%*%(data0^2) / den0 
+          var0=(m0.2-m0^2) 
+          var0 = ifelse(var0==0 , NA , var0)
+          var0[,numeric.vars] =  var0[,numeric.vars,drop = FALSE] * N0 / (N0-1)
+        }
+        
+        # this just handles that var.p is a vector
+        std.effect = sweep( m1-m0 , 2 , sqrt(var.p) , "/")
+      }
+    }else{ #ATT
       # same steps as with treated, but now for controls
-      w0 = w[treat==0,]
-      data0 = as.matrix(data[treat==0,])
+      w0 = w[in. & treat==0,,drop = FALSE]
+      data0 = as.matrix(data_[in. & treat==0,,drop = FALSE])
       index.0 = !is.na(data0)
       den0 = t(w0)%*%index.0
       data0[!index.0] = 0
@@ -64,75 +113,60 @@ calcES <- function(data,treat,w,numeric.vars,estimand,multinom=FALSE,sw=NULL,get
       # calculate mean among control
       m0 = t(w0)%*%data0 / den0
       
+      # calculate E[X^2] among treated
+      m1.2 =  t(w1)%*%(data1^2) / den1 
+      var1=(m1.2-m1^2) 
+      var1 = ifelse(var1==0 , NA , var1)
+      var1[,numeric.vars] =  var1[,numeric.vars,drop = FALSE] * N1 / (N1-1)
+      # var1[,] =  var1[,,drop = FALSE] * N1 / (N1-1)
+      
       if (get.means){
         # calculate E[X^2] among control
         m0.2 =  t(w0)%*%(data0^2) / den0 
         var0=(m0.2-m0^2) 
         var0 = ifelse(var0==0 , NA , var0)
-        var0[,numeric.vars] =  var0[,numeric.vars] * N0 / (N0-1)
+        var0[,numeric.vars] =  var0[,numeric.vars,drop = FALSE] * N0 / (N0-1)
+        # var0[,] =  var0[,,drop = FALSE] * N0 / (N0-1)
       }
       
-      # this just handles that var.p is a vector
-      std.effect = sweep( m1-m0 , 2 , sqrt(var.p) , "/")
+      std.effect = (m1-m0)/sqrt(var1)
     }
-  }else{ #ATT
-    # same steps as with treated, but now for controls
-    w0 = w[treat==0,]
-    data0 = as.matrix(data[treat==0,])
-    index.0 = !is.na(data0)
-    den0 = t(w0)%*%index.0
-    data0[!index.0] = 0
     
-    # calculate mean among control
-    m0 = t(w0)%*%data0 / den0
-    
-    # calculate E[X^2] among treated
-    m1.2 =  t(w1)%*%(data1^2) / den1 
-    var1=(m1.2-m1^2) 
-    var1 = ifelse(var1==0 , NA , var1)
-    var1[,numeric.vars] =  var1[,numeric.vars] * N1 / (N1-1)
     
     if (get.means){
-      # calculate E[X^2] among control
-      m0.2 =  t(w0)%*%(data0^2) / den0 
-      var0=(m0.2-m0^2) 
-      var0 = ifelse(var0==0 , NA , var0)
-      var0[,numeric.vars] =  var0[,numeric.vars] * N0 / (N0-1)
+      ## surveyglm always uses total sample, not number of non-NA
+      # N = nrow(data)
+      # X = cbind(1 , treat)
+      # index = !is.na(data)
+      # t.n <- NULL
+      # for (var in numeric.vars){
+      #   x = data[,var]
+      #   D=solve(t(X)%*%diag(w*index[,var])%*%X)
+      #   beta = D%*%t(X)%*%diag(w*index[,var])%*%x
+      #   resid = x - X%*%beta
+      #   a = sweep(X,1,resid*w*index[,var],"*")
+      #   a = beta[2]/sqrt( (D%*%t(a)%*%a%*%t(D)*N/(N-1))[2,2] )
+      #   t.n = rbind(t.n ,c(a , 1-2*pt(-abs(a) ,df=N-1 )) )
+      # }
+      # rownames(t.n) = numeric.vars
+      res = t(rbind(tx.mn=m1 , tx.sd=sqrt(var1), ct.mn=m0 , ct.sd=sqrt(var0) , std.eff.sz=std.effect ))
+      colnames(res) = c("tx.mn", "tx.sd",  "ct.mn", "ct.sd", "std.eff.sz")
+      rownames(res) <- paste0(g_name, colnames(std.effect))
+      #res[numeric.vars,c("tx.sd","ct.sd")] =  res[numeric.vars,c("tx.sd","ct.sd")]*sqrt(N/(N-1))
+      return(res)
+    }else{
+      colnames(std.effect) <- paste0(g_name, colnames(std.effect))
+      return(std.effect)
     }
     
-    std.effect = (m1-m0)/sqrt(var1)
-  }
-  
-  # scale by sample size for numeric -- this is the correction factor for the variance: N/(N-1)
-  # svyvar always uses actual sample size, not number of non-NA. 
-  # if (estimand=="ATE"){
-  #   N = nrow(data)
-  # }else{
-  #   N = sum(treat)
-  # }
-  # std.effect[,numeric.vars] = std.effect[,numeric.vars]* sqrt( (N-1) / N )
+  })
   
   if (get.means){
-    ## surveyglm always uses total sample, not number of non-NA
-    # N = nrow(data)
-    # X = cbind(1 , treat)
-    # index = !is.na(data)
-    # t.n <- NULL
-    # for (var in numeric.vars){
-    #   x = data[,var]
-    #   D=solve(t(X)%*%diag(w*index[,var])%*%X)
-    #   beta = D%*%t(X)%*%diag(w*index[,var])%*%x
-    #   resid = x - X%*%beta
-    #   a = sweep(X,1,resid*w*index[,var],"*")
-    #   a = beta[2]/sqrt( (D%*%t(a)%*%a%*%t(D)*N/(N-1))[2,2] )
-    #   t.n = rbind(t.n ,c(a , 1-2*pt(-abs(a) ,df=N-1 )) )
-    # }
-    # rownames(t.n) = numeric.vars
-    res = t(rbind(tx.mn=m1 , tx.sd=sqrt(var1), ct.mn=m0 , ct.sd=sqrt(var0) , std.eff.sz=std.effect ))
-    colnames(res) = c("tx.mn", "tx.sd",  "ct.mn", "ct.sd", "std.eff.sz")
-    #res[numeric.vars,c("tx.sd","ct.sd")] =  res[numeric.vars,c("tx.sd","ct.sd")]*sqrt(N/(N-1))
-    return(res)
-  }else{
-    return(std.effect)
+    out <- do.call("rbind", std.effect.list)
   }
+  else {
+    out <- do.call("cbind", std.effect.list)
+  }
+
+  return(out)
 }
--- R/calcKS.R
+++ R/calcKS.R
@@ -1,4 +1,13 @@
-calcKS <- function(data, w , treat , multinomATE=FALSE, sw=NULL ){
+calcKS <- function(data, w , treat , multinomATE=FALSE, sw=NULL, moderator.var = NULL){
+  
+  if (!is.null(moderator.var)) {
+    moderator = data[,moderator.var]
+    groups <- c("all", levels(moderator))
+    data <- data[,!colnames(data) %in% moderator.var, drop = FALSE]
+  }
+  else {
+    groups <- ""
+  }
   
   W1 = sweep(w , 1 , treat , "*") #apply(w,2,function(x) x*treat )
   if (multinomATE){
@@ -7,31 +16,58 @@ calcKS <- function(data, w , treat , multinomATE=FALSE, sw=NULL ){
     W0 = sweep(w , 1 , 1-treat , "*") #apply(w,2,function(x) x*(1-treat))
   }
   
-  ks.effect = NULL
-  for (j in 1:ncol(data)){
-    # order of the data, dropping NA. Only numeric variables have NAs.
-    index = order( data[,j] , na.last = NA)
+  ks.effect.list <- lapply(groups, function(g) {
     
-    # consider last occurance of unique values
-    dups = !duplicated(data[index,j],fromLast=T)
-    
-    if (multinomATE){
-      # handles the fact that W0 is a vector when multinomATE is true
-      dW = abs( sweep(apply(W1[index,,drop=FALSE] , 2 , function(y) cumsum(y)/sum(y) ) , 1 ,  cumsum(sw[index])/sum(sw[index]) , "-" ) )
-    }else{
-      dW = abs(apply(W1[index,,drop=FALSE] , 2 , function(y) cumsum(y)/sum(y) ) - apply(W0[index,,drop=FALSE] , 2 , function(y) cumsum(y)/sum(y) ))
+    if (g %in% c("", "all")) {
+      in. <- rep(TRUE, nrow(data))
+      data_g <- data
+      if (g == "") g_name <- ""
+      else g_name <- paste0(g, "_")
+    }
+    else {
+      in. <- moderator == g
+      #Drop moderator variable from subgroup balance
+      data_g <- data[in.,!colnames(data) %in% paste(moderator.var, levels(moderator), sep = ":"), drop = FALSE]
+      g_name <- paste0(g, "_")
     }
     
-    if ( all(data[,j] %in% c(0,1))){
-      # factors and NA are coded 0/1, and dW is sorted, so the ks statistic corresponds to last data point with 0
-      ks.effect = cbind(ks.effect , dW[sum(data[,j]==0),])
-    }else{  
-      # numeric ks is just the max
-      ks.effect = cbind(ks.effect , apply(dW[dups,,drop=FALSE] , 2 , max))
-    }
-  }
-  colnames(ks.effect) = colnames(data)
+    W1_g <- W1[in.,,drop = FALSE]
+    W0_g <- W0[in.,,drop = FALSE]
+    sw_g <- sw[in.]
     
-  return(ks.effect)  
+    ks.effect = NULL
+    for (j in 1:ncol(data_g)){
+      
+      # order of the data, dropping NA. Only numeric variables have NAs.
+      index = order(data_g[,j], na.last = NA)
+      
+      # consider last occurance of unique values
+      dups = !duplicated(data_g[index,j], fromLast=T)
+      
+      if (multinomATE){
+        # handles the fact that W0 is a vector when multinomATE is true
+        dW = abs(sweep(apply(W1_g[index,,drop=FALSE] , 2 , function(y) cumsum(y)/sum(y) ) , 1 ,  cumsum(sw_g[index])/sum(sw_g[index]) , "-" ) )
+      }else{
+        dW = abs(apply(W1_g[index,,drop=FALSE] , 2 , function(y) cumsum(y)/sum(y) ) - apply(W0_g[index,,drop=FALSE] , 2 , function(y) cumsum(y)/sum(y) ))
+      }
+      
+      if ( all(data_g[,j] %in% c(0,1))){
+        # factors and NA are coded 0/1, and dW is sorted, so the ks statistic corresponds to last data point with 0
+        ks.effect = cbind(ks.effect , dW[sum(data_g[,j]==0),])
+      }else{  
+        # numeric ks is just the max
+        ks.effect = cbind(ks.effect , apply(dW[dups,,drop=FALSE] , 2 , max))
+      }
+    }
+    
+    colnames(ks.effect) = paste0(g_name, colnames(data_g))
+    
+    return(ks.effect) 
+  })
+  
+  out <- do.call("cbind", ks.effect.list)
+
+  return(out)
+   
 }
 
