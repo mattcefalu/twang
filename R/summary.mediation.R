6bb3de956f5e17eab54147a7d459b29cdf23de08
jbiggsets <jbiggs@ets.org>
2019-09-30 09:23:59 -0400

    Update the colors in the plot function, and the balance tables in the summary function.

--- R/summary.mediation.R
+++ R/summary.mediation.R
@@ -6,18 +6,25 @@
 #' @method summary mediation
 #' @export
 summary.mediation <- function(object, ...) {
-  
+
+  column_order <- c('model', 'tx.mn', 'tx.sd', 'ct.mn', 'ct.sd',
+                    'std.eff.sz', 'stat', 'p', 'ks', 'ks.pval')
+
+  # grab the balance table, reorder the columns, and split into
+  # multiple data frames using the `model` column
   balance_tables <- bal_table(object)
-  
+  balance_tables <- balance_tables[,column_order]
+  balance_tables <- split(balance_tables, balance_tables$model)
+
   # loop through the effects and put them
   # into a single data frame, then print
   effects_names <- sapply(names(object), function (x) grepl('_effects', x))
   if (any(effects_names)) {
-    
+
     effects <- object[effects_names]
     results <- matrix(NA, nrow = 7, ncol = length(effects))
     for (effect_idx in 1:length(effects)) {
-      
+
       if (effect_idx == 1) {
         effects_columns <- names(effects[[effect_idx]])
       }
@@ -25,14 +32,14 @@ summary.mediation <- function(object, ...) {
       for (idx in 1:7) {
         results[idx, effect_idx] <- effects[[effect_idx]][[idx]]
       }
-      
+
     }
     results_table <- as.data.frame(results)
     colnames(results_table) <- names(effects)
     rownames(results_table) <- effects_columns
-    
+
     return(list('results_table' = results_table, 'balance_tables' = balance_tables))
-    
+
   }
-  
+
 }
